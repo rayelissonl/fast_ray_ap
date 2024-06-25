301298e4bdea11314408738cda1f2752837cfc53
RAYELISSON <rayelissonl@gmail.com>
2024-06-25 19:35:16 -0300

    as config do repo

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_ray_ap.app import app
+
+
+def test_read_root_deve_retornar_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {'messsage': 'Ola Mundo!'}
