6c2fbf3912ec6c70446cef6ad80c11a39e84ee9c
RAYELISSON <rayelissonl@gmail.com>
2024-06-29 23:09:29 -0300

    parte do dia do comeco do crud

--- fast_ray_ap/app.py
+++ fast_ray_ap/app.py
@@ -1,8 +1,15 @@
 from http import HTTPStatus
+from http.client import HTTPException
 
 from fastapi import FastAPI
 
-from fast_ray_ap.schemas import UserDB, UserPublic, UserSchema
+from fast_ray_ap.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
@@ -21,3 +28,32 @@ def create_user(user: UserSchema):
     database.append(user_with_id)
 
     return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,8 @@
 from http import HTTPStatus
 
-import pytest
-from fastapi.testclient import TestClient
-
-from fast_ray_ap.app import app
-
-
-@pytest.fixture()
-def client():
-    return TestClient(app)
+# @pytest.fixture()
+# def client():
+# return TestClient(app)
 
 
 def test_read_root_deve_retornar_ola_mundo(client):
@@ -36,3 +30,40 @@ def test_create_user(client):
         'email': 'testray@example.com',
         'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testray',
+                'email': 'testray@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'testray',
+            'password': '123',
+            'email': 'testray@examrle.com',
+            'id': 1,
+        },
+    )
+    assert response.json() == {
+        'username': 'testray',
+        'email': 'testray@examrle.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
