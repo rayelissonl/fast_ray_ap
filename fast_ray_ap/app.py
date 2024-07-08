8fb79bf181b30cd6eac5bd8688a227ad04c5e475
RAYELISSON <rayelissonl@gmail.com>
2024-07-08 00:03:45 -0300

    parte do dia 08/07/24

--- fast_ray_ap/app.py
+++ fast_ray_ap/app.py
@@ -1,13 +1,14 @@
 from http import HTTPStatus
-# from http.client import HTTPException
 
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine
+# from http.client import HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from fast_ray_ap.database import get_session
+from fast_ray_ap.models import User
 from fast_ray_ap.schemas import (
     Message,
-    UserDB,
     UserList,
     UserPublic,
     UserSchema,
@@ -15,58 +16,82 @@ from fast_ray_ap.schemas import (
 
 app = FastAPI()
 
-database = []
+# database = []
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'messsage': 'Ola Mundo!'}
+    return {'message': 'Olá Mundo!'}
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
-
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where(
-                (User.username == user.username) | (User.email == user.email)
-            )
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
         )
+    )
 
-        if db_user:
-            return 'DEU RUIM'
-        # user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)  # (2)!
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-        # database.append(user_with_id)
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
 
-        # return user_with_id
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id < 1 or user_id > len(database):
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    user_with_id = UserDB(id=user_id, **user.model_dump())
 
-    database[user_id - 1] = user_with_id
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
 
-    return user_with_id
+    return db_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    del database[user_id - 1]
+
+    session.delete(db_user)
+    session.commit()
 
     return {'message': 'User deleted'}
