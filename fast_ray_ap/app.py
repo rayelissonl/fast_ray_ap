157d87c11eec74862d734db2849acda14b70c667
RAYELISSON <rayelissonl@gmail.com>
2024-07-14 00:15:57 -0300

    concertando o erro

--- fast_ray_ap/app.py
+++ fast_ray_ap/app.py
@@ -1,29 +1,24 @@
 from http import HTTPStatus
 
-# from http.client import HTTPException
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_ray_ap.database import get_session
 from fast_ray_ap.models import User
-from fast_ray_ap.schemas import (
-    Message,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
+from fast_ray_ap.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
-# database = []
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'Hello World!'}
 
 
+@app.get
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
@@ -45,45 +40,71 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
             )
 
     db_user = User(
-        username=user.username, password=user.password, email=user.email
+        username=user.username, email=user.email, password=user.password
     )
 
     session.add(db_user)
     session.commit()
     session.refresh(db_user)
+    return db_user
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users(
+    skip: int = 0,
+    limit: int = 100,
+    session: Session = Depends(get_session),
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+
+    return {
+        'users': users,
+    }
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
 
     return db_user
 
 
-@app.get('/users/', response_model=UserList)
-def read_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
-):
-    users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {'users': users}
-
-
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
 ):
     db_user = session.scalar(select(User).where(User.id == user_id))
+
     if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     db_user.username = user.username
-    db_user.password = user.password
     db_user.email = user.email
+    db_user.password = user.password
+
+    # session.add(db_user) # Pode-se usar para padronização
     session.commit()
     session.refresh(db_user)
-
     return db_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
     if not db_user:
