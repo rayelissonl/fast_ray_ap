b2a912acfd043893f7cd18c4b3042cf4bacec60a
RAYELISSON <rayelissonl@gmail.com>
2024-07-24 00:19:49 -0300

    refatoracao do codigo

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,5 @@
 from http import HTTPStatus
 
-from fast_ray_ap.schemas import UserPublic
-
 # @pytest.fixture()
 # def client():
 # return TestClient(app)
@@ -12,72 +10,3 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!'}
-
-
-def test_create_user(client):
-    response = client.post(
-        '/users/',
-        json={
-            'username': 'testray',
-            'email': 'testray@example.com',
-            'password': 'secret',
-        },
-    )
-    assert response.status_code == HTTPStatus.CREATED
-    assert response.json() == {
-        'username': 'testray',
-        'email': 'testray@example.com',
-        'id': 1,
-    }
-
-
-def test_read_users(client):
-    response = client.get('/users/')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'users': []}
-
-
-def test_read_users_with_users(client, user):
-    user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/')
-    assert response.json() == {'users': [user_schema]}
-
-
-def test_update_user(client, user, token):
-    response = client.put(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
-        json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
-        },
-    )
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': user.id,
-    }
-
-
-def test_delete_user(client, user, token):
-    response = client.delete(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
-    )
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'User deleted'}
-
-
-def test_get_token(client, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},
-    )
-
-    token = response.json()
-    assert response.status_code == HTTPStatus.OK
-    assert 'access_token' in token
-    assert 'token_type' in token
